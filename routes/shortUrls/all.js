540e907abc16926f90406ef4983c8771f4f856a4
Pallavi Subramanya <psubramanya@babbel.com>
2022-03-13 10:33:04 +0100

    feat: update all route to fetch visit details
    - updated all route path to fetch visit collection with url populated

--- routes/shortUrls/all.js
+++ routes/shortUrls/all.js
@@ -2,21 +2,15 @@ import { fetchUrls } from '../../services/shortUrls';
 
 const allRouter = async (req, res, next) => {
   try {
-    const shortUrls = await fetchUrls();
+    const urls = await fetchUrls();
 
-    if (shortUrls && shortUrls.length) {
-      const response = shortUrls.map(({ full, short, clicks }) => ({
-        full,
-        short,
-        clicks,
-      }));
-
-      return res.status(200).json(response);
+    if (urls && urls.length) {
+      return res.status(200).json(urls);
     } else {
       return res.status(200).json({ message: 'No urls' });
     }
   } catch (error) {
-    const err = new Error(`An error occurred in finding the urls: ${error}`);
+    const err = new Error(`An error occurred in fetching the urls: ${error}`);
     err.status = 500;
     return next(err);
   }
--- routes/shortUrls/all.test.js
+++ routes/shortUrls/all.test.js
@@ -2,36 +2,41 @@ import request from 'supertest';
 import app from '../../app';
 import * as mockingoose from 'mockingoose';
 
-import ShortUrlModel from '../../models/shortUrls';
+import { Visit } from '../../models/shortUrls';
 
 afterEach(() => {
   mockingoose.resetAll();
 });
 
-const mockedUrls = [
+const mockedVisits = [
   {
-    full: 'https://expressjs.com/en/guide/routing.html',
-    short: 'tier.app-PFaLqVSwV',
-    clicks: 0,
+    count: 2,
+    url: {
+      full: 'https://www.monkeyuser.com/2019/bug-fixing-ways/1',
+      short: 'tier.app-atrrKwWfW',
+    },
   },
   {
-    full: 'https://expressjs.com/en/guide/routing.html',
-    short: 'tier.app-PFaLqVSwV',
-    clicks: 0,
+    count: 0,
+    url: {
+      full: 'https://www.monkeyuser.com/2019/bug-fixing-ways/2',
+      short: 'tier.app-atrrKwWfW',
+    },
   },
 ];
 
 describe('GET', () => {
   describe('when short urls are present', () => {
     it('returns 200 with list of urls', async () => {
-      expect.assertions(2);
+      expect.assertions(3);
 
-      mockingoose(ShortUrlModel).toReturn(mockedUrls, 'find');
+      mockingoose(Visit).toReturn(mockedVisits, 'find');
 
       const response = await request(app).get('/shortUrls');
 
       expect(response.statusCode).toEqual(200);
-      expect(response.body).toEqual(mockedUrls);
+      expect(response.body[0]).toMatchObject({ count: mockedVisits[0].count });
+      expect(response.body[1]).toMatchObject({ count: mockedVisits[1].count });
     });
   });
 
@@ -39,7 +44,7 @@ describe('GET', () => {
     it('returns 200 with message', async () => {
       expect.assertions(2);
 
-      mockingoose(ShortUrlModel).toReturn(null, 'find');
+      mockingoose(Visit).toReturn(null, 'find');
 
       const response = await request(app).get('/shortUrls');
 
